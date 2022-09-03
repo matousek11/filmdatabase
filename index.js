a2c9d685c8f13393d38410ee00e03cd3be1a29fe
matousek11 <matousek1199@gmail.com>
2022-09-03 16:19:16 +0200

    Vytvoření post metody

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+app.use(express.json())
 
 const movies = [
   { id: 1, name: 'Kill Bill', year: 2003 },
@@ -20,6 +21,16 @@ app.get('/api/movies/:id', (req, res) => {
   } else {
     res.status(404).send('Film nebyl nalezen.')
   }
+})
+
+app.post('/api/movies', (req, res) => {
+  const movie = {
+    id: movies.length + 1,
+    name: req.body.name,
+    year: req.body.year,
+  }
+  movies.push(movie)
+  res.send(movie)
 })
 
 app.listen(3000, () => console.log('Listening on port 3000...'))
